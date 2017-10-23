1f84e2aa39c72d525ba14ad81c57ecd622ab2a14
Intera-PC <Intera-PC@LAPTOP-MPVMO4BN.bs.br.bsch>
2017-10-23 11:52:55 -0200

    Threads + sockets

---
+++ servidor-tarefas/src/br/com/alura/cliente/ClienteTarefas.java
@@ -0,0 +1,19 @@
+package br.com.alura.cliente;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class ClienteTarefas {
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		Socket socket = new Socket("localhost", 12345);
+		
+		System.out.println("conexão estabelecida");
+		
+		Scanner teclado = new Scanner(System.in);
+		teclado.nextLine();
+		
+		socket.close();
+	}
+}
---
+++ servidor-tarefas/src/br/com/alura/servidor/ServidorTarefas.java
@@ -0,0 +1,20 @@
+package br.com.alura.servidor;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorTarefas {
+	public static void main(String[] args) throws IOException {
+		System.out.println("--- Iniciando servidor ---");
+		ServerSocket servidor = new ServerSocket(12345);
+		
+		while(true) {
+			Socket socket = servidor.accept();
+			DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
+			Thread thread = new Thread(distribuirTarefas);
+			thread.start();
+			System.out.println("Aceitando novo cliente na porta: " + socket.getPort());
+		}
+	}
+}
