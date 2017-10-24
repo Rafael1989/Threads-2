239be14296e815fea91a38bfb1bfea02edd1e609
Intera-PC <Intera-PC@LAPTOP-MPVMO4BN.bs.br.bsch>
2017-10-24 13:16:06 -0200

    comando fim para finalizar o servidor

--- servidor-tarefas/src/br/com/alura/servidor/DistribuirTarefas.java
+++ servidor-tarefas/src/br/com/alura/servidor/DistribuirTarefas.java
@@ -7,9 +7,11 @@ import java.util.Scanner;
 public class DistribuirTarefas implements Runnable{
 
 	private Socket socket;
+	private ServidorTarefas servidorTarefas;
 	
-	public DistribuirTarefas(Socket socket) {
+	public DistribuirTarefas(Socket socket, ServidorTarefas servidorTarefas) {
 		this.socket = socket;
+		this.servidorTarefas = servidorTarefas;
 	}
 	
 	@Override
@@ -33,6 +35,10 @@ public class DistribuirTarefas implements Runnable{
 				case "c2":
 					saidaCliente.println("Confirmação do comando c2");
 					break;
+				case "fim":
+					saidaCliente.println("Desligando o servidor");
+					servidorTarefas.parar();
+					return;
 				default:
 					saidaCliente.println("Comando não encontrado");
 				}
--- servidor-tarefas/src/br/com/alura/servidor/ServidorTarefas.java
+++ servidor-tarefas/src/br/com/alura/servidor/ServidorTarefas.java
@@ -3,21 +3,47 @@ package br.com.alura.servidor;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ServidorTarefas {
-	public static void main(String[] args) throws IOException {
-		System.out.println("--- Iniciando servidor ---");
-		ServerSocket servidor = new ServerSocket(12345);
-		
-		ExecutorService threadPool = Executors.newFixedThreadPool(2);
-		
-		while(true) {
-			Socket socket = servidor.accept();
-			DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
-			threadPool.execute(distribuirTarefas);
-			System.out.println("Aceitando novo cliente na porta: " + socket.getPort());
+	
+	private ServerSocket servidor;
+	private ExecutorService threadPool;
+	private AtomicBoolean estaRodando;
+	
+	public ServidorTarefas() throws IOException {
+		System.out.println("---- Iniciando servidor ----");
+		this.servidor = new ServerSocket(12345);
+		this.threadPool = Executors.newCachedThreadPool();
+		this.estaRodando = new AtomicBoolean(true);
+	}
+	
+	public void rodar() throws IOException {
+		while(this.estaRodando.get()) {
+			try {
+				Socket socket = this.servidor.accept();
+				System.out.println("Aceitando novo cliente na porta " + socket.getPort());
+				
+				DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket, this);
+				this.threadPool.execute(distribuirTarefas);
+			}catch(SocketException e) {
+				System.out.println("SocketException, está rodando? " + this.estaRodando);
+			}
 		}
+	}
+	
+	public void parar() throws IOException {
+		System.out.println("Parando o servidor");
+		this.estaRodando.set(false);
+		this.threadPool.shutdown();
+		this.servidor.close();
+	}
+	
+	public static void main(String[] args) throws IOException {
+		ServidorTarefas servidor = new ServidorTarefas();
+		servidor.rodar();
 	}
 }
